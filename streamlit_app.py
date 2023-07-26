c236fea7a7334b4afe56841d8f94889ecde8434a
dawnberdan <140385784+dawnberdan@users.noreply.github.com>
2023-07-26 13:19:04 +0800

    Update streamlit_app.py
    data frame

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,9 @@ streamlit.text(fruityvice_response.json()) # just writes the data to the screen
 # take the json version of the response and normalize it 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
+#output it the screen as a table
+streamlit.dataframe(fruityvice_normalized)
+
 
 
 
